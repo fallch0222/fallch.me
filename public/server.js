8cd752a765cf7131021d7ec983d086f3c197c1bf
fallch0222 <56762890+fallch0222@users.noreply.github.com>
2022-10-25 00:34:15 +0900

    Update server.js

--- public/server.js
+++ public/server.js
@@ -2,8 +2,11 @@
 function server() {
   var app = express();
   app.use(express.static(__dirname + '/public'));
+  var port = normalizePort('80');
+  app.set('port', port);
   app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
   });
   return app;
 }
+
